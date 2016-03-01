0da9a50df14af6378067cb42cd8b88ade69b7278
Joao Matos Silva <j.silva@mindbus.nl>
2016-03-01 23:44:05 +0000

    Added Initial Version

---
+++ src/Mindbus.Constants.Time/Time.cs
@@ -0,0 +1,39 @@
+﻿namespace Mindbus.Constants.Time
+{
+    /// <summary>
+    /// A Set of commonly used Time constants
+    /// </summary>
+    public static class Time
+    {
+        /// <summary>
+        /// Commonly used Time constants values in Seconds
+        /// </summary>
+        public static class InSeconds
+        {
+            public const int OneSecond = 1;
+            public const int FiveSeconds = 5;
+            public const int OneMinute = OneSecond*60;
+            public const int OneHour = OneMinute*60;
+            public const int OneDay = OneHour*24;
+        }
+
+        /// <summary>
+        /// Commonly used Time constants values in Minutes
+        /// </summary>
+        public static class InMinutes
+        {
+            public const int OneMinute = 1;
+            public const int OneHour = OneMinute*60;
+            public const int OneDay = OneHour*24;
+        }
+
+        /// <summary>
+        /// Commonly used Time constants values in Hours 
+        /// </summary>
+        public static class InHours
+        {
+            public const int OneHour = 1;
+            public const int OneDay = 24;
+        }
+    }
+}
---
+++ test/Mindbus.Constants.Time.Tests/TimeTests.cs
@@ -0,0 +1,32 @@
+﻿using Xunit;
+
+namespace Mindbus.Constants.Time.Tests
+{
+    public class TimeTests
+    {
+        [Fact]
+        public void InSeconds()
+        {
+            Assert.Equal(1, Time.InSeconds.OneSecond);
+            Assert.Equal(5, Time.InSeconds.FiveSeconds);
+            Assert.Equal(60, Time.InSeconds.OneMinute);
+            Assert.Equal(3600, Time.InSeconds.OneHour);
+            Assert.Equal(86400, Time.InSeconds.OneDay);
+        }
+
+        [Fact]
+        public void InMinutes()
+        {
+            Assert.Equal(1, Time.InMinutes.OneMinute);
+            Assert.Equal(60, Time.InMinutes.OneHour);
+            Assert.Equal(1440, Time.InMinutes.OneDay);
+        }
+
+        [Fact]
+        public void InHours()
+        {
+            Assert.Equal(1, Time.InHours.OneHour);
+            Assert.Equal(24, Time.InHours.OneDay);
+        }
+    }
+}
